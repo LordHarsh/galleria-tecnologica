3c139cfa04f571fa0a99adab99f69f3180cd4868
Harsh Banka <harshbanka321@gmail.com>
2023-06-21 17:24:10 +0530

    added prose class

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -24,9 +24,9 @@ export default async function RootLayout({
         <header className="flex items-center justify-between">
           <Link
             href="/"
-            className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold"
+            className="md:text-3xl lg:text-4xl bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold"
           >
-            Harsh
+            Galleria Technologica
           </Link>
           <div className="flex items-center gap-5 text-sm text-grey-600">
             {pages.map((page) => (
@@ -36,7 +36,7 @@ export default async function RootLayout({
             ))}
           </div>
         </header>
-        <main className="py-20"> {children}</main>
+        <main className="py-20 prose"> {children}</main>
       </body>
     </html>
   );
