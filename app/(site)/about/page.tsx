0bed76f55057d9632ba4916d3e903dc094962e08
Harsh Banka <harshbanka321@gmail.com>
2023-07-17 00:35:01 +0530

    fix

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,38 +1,38 @@
-// import Link from 'next/link';
-// import Image from 'next/image';
+import Link from 'next/link';
+import Image from 'next/image';
 
-// const About = () => {
-//   return (
-//     <div className="max-w-3xl mx-auto">
-//       <div className="bg-white rounded-lg shadow-md p-8 mb-8">
-//         <h1 className="text-3xl font-bold mb-4">About Me</h1>
-//         <p className="text-gray-600">
-//           Hello! I&apos;m Harsh, and I have a strong interest in AI/ML and web development. I enjoy exploring the intersection of these fields and applying them to create innovative solutions. In addition to my technical pursuits, I am also passionate about continuously learning and improving my skills.
-//         </p>
-//       </div>
+const About = () => {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <div className="bg-white rounded-lg shadow-md p-8 mb-8">
+        <h1 className="text-3xl font-bold mb-4">About Me</h1>
+        <p className="text-gray-600">
+          Hello! I&apos;m Harsh, and I have a strong interest in AI/ML and web development. I enjoy exploring the intersection of these fields and applying them to create innovative solutions. In addition to my technical pursuits, I am also passionate about continuously learning and improving my skills.
+        </p>
+      </div>
 
-//       <div className="bg-white rounded-lg shadow-md p-8 mb-8">
-//         <h2 className="text-xl font-bold mb-4">Education</h2>
-//         <ul className="list-disc list-inside">
-//           <li>Junior School: [Name of Junior School]</li>
-//           <li>High School: [Name of High School]</li>
-//           <li>Current College: [Name of Current College]</li>
-//         </ul>
-//       </div>
+      <div className="bg-white rounded-lg shadow-md p-8 mb-8">
+        <h2 className="text-xl font-bold mb-4">Education</h2>
+        <ul className="list-disc list-inside">
+          <li>Junior School: [Name of Junior School]</li>
+          <li>High School: [Name of High School]</li>
+          <li>Current College: [Name of Current College]</li>
+        </ul>
+      </div>
 
-//       <div className="bg-white rounded-lg shadow-md p-8 mb-8">
-//         <h2 className="text-xl font-bold mb-4">Connect with Me</h2>
-//         <div className="flex flex-wrap">
-//           {/* <SocialLink icon="/discord-icon.png" href="[Discord URL]" label="Discord" />
-//           <SocialLink icon="/linkedin-icon.png" href="[LinkedIn URL]" label="LinkedIn" />
-//           <SocialLink icon="/github-icon.png" href="[GitHub URL]" label="GitHub" />
-//           <SocialLink icon="/leetcode-icon.png" href="[LeetCode URL]" label="LeetCode" />
-//           <SocialLink icon="/email-icon.png" href="mailto:[Your Email]" label="Email" /> */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="bg-white rounded-lg shadow-md p-8 mb-8">
+        <h2 className="text-xl font-bold mb-4">Connect with Me</h2>
+        <div className="flex flex-wrap">
+          {/* <SocialLink icon="/discord-icon.png" href="[Discord URL]" label="Discord" />
+          <SocialLink icon="/linkedin-icon.png" href="[LinkedIn URL]" label="LinkedIn" />
+          <SocialLink icon="/github-icon.png" href="[GitHub URL]" label="GitHub" />
+          <SocialLink icon="/leetcode-icon.png" href="[LeetCode URL]" label="LeetCode" />
+          <SocialLink icon="/email-icon.png" href="mailto:[Your Email]" label="Email" /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // const SocialLink = ({ icon, href, label }) => {
 //   return (
@@ -48,4 +48,4 @@
 //   );
 // };
 
-// export default About;
+export default About;
